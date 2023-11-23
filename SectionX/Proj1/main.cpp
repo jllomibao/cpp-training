dd39cd862241002377e67e6bbebc30ef0400a9e8
Jeffrey Lomibao <jllomibao@gmail.com>
2023-11-23 12:54:21 -0800

    Initial files setup for development

---
+++ SectionX/Proj1/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Hello from Proj1" << endl;
+  return 0;
+}
---
+++ SectionX/Proj2/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Hello from Proj2" << endl;
+  return 0;
+}
