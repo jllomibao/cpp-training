59daa89094c4ef0d0848147f2928de865066ea4e
Jeffrey Lomibao <jllomibao@gmail.com>
2023-12-23 07:51:34 -0800

    Create debug configuration

--- QuickStart/Proj2/main.cpp
+++ QuickStart/Proj2/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
+  int fav_number {100};
   cout << "Hello from Proj2" << endl;
+  cout << "Enter your favorite number: ";
+  cin >> fav_number;
+  cout << "Your favorite number is: " << fav_number << endl;
   return 0;
 }
